156fb79566773bd6a03c4a352106a5b0366c1f9a
@Ky777 <kondr777@live.ru>
2026-05-15 20:35:16 +0300

    add: 2 #1

---
+++ FirstGitHubActions/FirstGitHubActions.Client/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstGitHubActions.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstGitHubActions.Library.Info.GetOS());
+        }
+    }
+}
